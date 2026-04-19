2f0286ce9e9da4c63b786ea609bf2f334a7fb4af
MaryVildosola <maryvildosola99@gmail.com>
2026-04-19 21:51:30 +0800

    edit models

--- app/Models/Pen.php
+++ app/Models/Pen.php
@@ -9,7 +9,9 @@ use App\Models\Pig;
 class Pen extends Model
 {
     protected $fillable = [
-        'name',
+        'name', 'section', 'status', 'healthy_pigs', 'sick_pigs', 'avg_weight',
+        'target_weight', 'batch_cost', 'feed_cons', 'profit_margin',
+        'progress', 'start_date', 'end_date'
     ];
 
     public function pigs()
