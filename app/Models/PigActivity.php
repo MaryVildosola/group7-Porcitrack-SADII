65ff911070d9776bbabb328c13b5e64c429eec12
MaryVildosola <maryvildosola99@gmail.com>
2026-04-24 13:41:18 +0800

    modification

--- app/Models/PigActivity.php
+++ app/Models/PigActivity.php
@@ -12,6 +12,17 @@ class PigActivity extends Model
         'type',
         'action',
         'details',
+        'is_critical_alert',
+        'admin_response',
+        'new_health_status',
+        'new_feeding_status',
+        'acknowledged_at',
+        'acknowledged_by',
+    ];
+
+    protected $casts = [
+        'is_critical_alert' => 'boolean',
+        'acknowledged_at' => 'datetime',
     ];
 
     public function pig()
@@ -22,5 +33,15 @@ class PigActivity extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function acknowledgedBy()
+    {
+        return $this->belongsTo(User::class, 'acknowledged_by');
+    }
+
+    public function scopeUnacknowledgedAlerts($query)
+    {
+        return $query->where('is_critical_alert', true)->whereNull('acknowledged_at');
     }
 }
