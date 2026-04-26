f395e9920dffd60ffa4ed428bf9b23a12d591aaa
D.raven <143114098+D-ravenn@users.noreply.github.com>
2026-04-26 19:42:17 +0800

    updated

--- database/migrations/2026_04_15_013614_create_pigs_table.php
+++ database/migrations/2026_04_15_013614_create_pigs_table.php
@@ -9,16 +9,31 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('pigs', function (Blueprint $table) {
-            $table->id();
-            $table->string('tag')->unique();
-            $table->foreignId('pen_id')->constrained('pens')->onDelete('cascade');
-            $table->date('birth_date')->nullable();
-            $table->timestamps();
-        });
-    }
+public function up(): void
+{
+    Schema::create('pigs', function (Blueprint $table) {
+        $table->id();
+        $table->string('tag')->unique();
+
+        $table->string('breed')->nullable();
+
+        $table->foreignId('pen_id')->constrained('pens')->onDelete('cascade');
+
+        $table->string('status')->default('active');
+
+        $table->date('birth_date')->nullable();
+
+        // FULL TRACKING
+        $table->integer('weight')->nullable();
+        $table->string('health_status')->default('Healthy');
+
+        $table->integer('bcs_score')->default(3);
+        $table->string('feeding_status')->default('Normal');
+        $table->string('symptoms')->nullable();
+
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
