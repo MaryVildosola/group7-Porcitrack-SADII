541104430df9edab95df96753e92eb2d05954379
D.raven <143114098+D-ravenn@users.noreply.github.com>
2026-04-26 19:40:23 +0800

    add user roles

--- database/migrations/2026_04_13_150258_add_role_to_users_table.php
+++ database/migrations/2026_04_13_150258_add_role_to_users_table.php
@@ -9,20 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('farm_worker');
-        });
-    }
+public function up(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->string('role')->default('farm_worker');
+        $table->string('photo')->nullable();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
-    }
+public function down(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn(['role', 'photo']);
+    });
+}
 };
