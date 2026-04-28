fe03d77b3b32f13db9bbf83fe4a49707f6bc31da
kayiieee <s.kapecomission@usm.edu.ph>
2026-04-29 04:13:30 +0800

    edits

--- app/Models/Pen.php
+++ app/Models/Pen.php
@@ -11,7 +11,7 @@ class Pen extends Model
     protected $fillable = [
         'name', 'section', 'status', 'healthy_pigs', 'sick_pigs', 'avg_weight',
         'target_weight', 'batch_cost', 'feed_cons', 'profit_margin',
-        'progress', 'start_date', 'end_date'
+        'progress', 'start_date', 'end_date', 'assigned_to'
     ];
 
     public function pigs()
@@ -22,5 +22,10 @@ class Pen extends Model
     public function feedConsumptions()
     {
         return $this->hasMany(FeedConsumption::class);
+    }
+
+    public function assignedPersonnel()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
     }
 }
